7bed955872f19d19d9041e87827f38d6549917db
Dmytro Vasyliev <dvasyliev.dev@gmail.com>
2022-08-02 14:27:46 +0200

    fixed mistmach between dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import stylelint from 'vite-plugin-stylelint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [vue(), eslint(), stylelint()]
 })
