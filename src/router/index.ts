d462cb3f1507b414755dd01589b683d2c018ff00
Dmytro Vasyliev <dvasyliev.dev@gmail.com>
2022-08-08 12:20:09 +0200

    add deploy to gh-pages using github actions

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { routes } from './routes'
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
